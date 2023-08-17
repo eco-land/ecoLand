4a90943f961a2ce64b58f3af423e86e15f194672
asma laptop <asmaalashwali@hotmail.com>
2023-08-18 00:04:24 +0300

    user contracts added

--- js/main.js
+++ js/main.js
@@ -1,287 +1,59 @@
-(function() {
-  "use strict";
+document.addEventListener("DOMContentLoaded", () => {
+  document.getElementById("btnLogin").addEventListener("click", () => {
+    const id = document.getElementById("userID").value;
+    const pas = document.getElementById("userPassword").value;
+    getUserID(id, pas);
+  });
 
-  /**
-   * Easy selector helper function
-   */
-  const select = (el, all = false) => {
-    el = el.trim()
-    if (all) {
-      return [...document.querySelectorAll(el)]
-    } else {
-      return document.querySelector(el)
-    }
-  }
+  document.getElementById("btnSignOut").addEventListener("click", () => {
+    userId = null;
+    arr = [];
+    console.log(userId);
+  });
+});
 
-  /**
-   * Easy event listener function
-   */
-  const on = (type, el, listener, all = false) => {
-    let selectEl = select(el, all)
-    if (selectEl) {
-      if (all) {
-        selectEl.forEach(e => e.addEventListener(type, listener))
+// ---------------- start user api fetch-------------------
+var userId;
+var arr = [];
+function getUserID(id, password) {
+  fetch("https://64d485a8b592423e4694350c.mockapi.io/api/v1/nafathUsers/")
+    .then((response) => response.json())
+    .then((data) => {
+      const user = data.find(
+        (user) => user.id === id && user.password === password
+      );
+      if (user) {
+        userId = user.id;
+        getContracts(user.id); // Fetch the contracts data
+        // Store the fetched data in the localStorage
+
+        // Redirect after the fetch completes & our data is stored
+        location.href = "./userAccount.html";
       } else {
-        selectEl.addEventListener(type, listener)
+        console.log(`Error in Passing the data`);
       }
-    }
+    });
+}
+
+function getContracts(id) {
+  fetch(
+    `https://64d485a8b592423e4694350c.mockapi.io/api/v1/nafathUsers/${id}/contracts`
+  )
+    .then((response) => response.json())
+    .then((data) => {
+      arr = data; // Replace existing array with fetched data
+      localStorage.setItem("contractsData", JSON.stringify(arr));
+      console.log(arr);
+    });
+}
+//   ---------------- end user api fetch-------------------
+// Retrieve the stored data from the localStorage
+document.getElementById("userData").addEventListener("click", () => {
+  const storedData = localStorage.getItem("contractsData");
+  if (storedData) {
+    const retrievedData = JSON.parse(storedData);
+    console.log(retrievedData); // Verify the retrieved data.
+  } else {
+    console.log("No stored data found!");
   }
-
-  /**
-   * Easy on scroll event listener 
-   */
-  const onscroll = (el, listener) => {
-    el.addEventListener('scroll', listener)
-  }
-
-  /**
-   * Navbar links active state on scroll
-   */
-  let navbarlinks = select('#navbar .scrollto', true)
-  const navbarlinksActive = () => {
-    let position = window.scrollY + 200
-    navbarlinks.forEach(navbarlink => {
-      if (!navbarlink.hash) return
-      let section = select(navbarlink.hash)
-      if (!section) return
-      if (position >= section.offsetTop && position <= (section.offsetTop + section.offsetHeight)) {
-        navbarlink.classList.add('active')
-      } else {
-        navbarlink.classList.remove('active')
-      }
-    })
-  }
-  window.addEventListener('load', navbarlinksActive)
-  onscroll(document, navbarlinksActive)
-
-  /**
-   * Scrolls to an element with header offset
-   */
-  const scrollto = (el) => {
-    let header = select('#header')
-    let offset = header.offsetHeight
-
-    if (!header.classList.contains('header-scrolled')) {
-      offset -= 20
-    }
-
-    let elementPos = select(el).offsetTop
-    window.scrollTo({
-      top: elementPos - offset,
-      behavior: 'smooth'
-    })
-  }
-
-  /**
-   * Toggle .header-scrolled class to #header when page is scrolled
-   */
-  let selectHeader = select('#header')
-  if (selectHeader) {
-    const headerScrolled = () => {
-      if (window.scrollY > 100) {
-        selectHeader.classList.add('header-scrolled')
-      } else {
-        selectHeader.classList.remove('header-scrolled')
-      }
-    }
-    window.addEventListener('load', headerScrolled)
-    onscroll(document, headerScrolled)
-  }
-
-  /**
-   * Back to top button
-   */
-  let backtotop = select('.back-to-top')
-  if (backtotop) {
-    const toggleBacktotop = () => {
-      if (window.scrollY > 100) {
-        backtotop.classList.add('active')
-      } else {
-        backtotop.classList.remove('active')
-      }
-    }
-    window.addEventListener('load', toggleBacktotop)
-    onscroll(document, toggleBacktotop)
-  }
-
-  /**
-   * Mobile nav toggle
-   */
-  on('click', '.mobile-nav-toggle', function(e) {
-    select('#navbar').classList.toggle('navbar-mobile')
-    this.classList.toggle('bi-list')
-    this.classList.toggle('bi-x')
-  })
-
-  /**
-   * Mobile nav dropdowns activate
-   */
-  on('click', '.navbar .dropdown > a', function(e) {
-    if (select('#navbar').classList.contains('navbar-mobile')) {
-      e.preventDefault()
-      this.nextElementSibling.classList.toggle('dropdown-active')
-    }
-  }, true)
-
-  /**
-   * Scrool with ofset on links with a class name .scrollto
-   */
-  on('click', '.scrollto', function(e) {
-    if (select(this.hash)) {
-      e.preventDefault()
-
-      let navbar = select('#navbar')
-      if (navbar.classList.contains('navbar-mobile')) {
-        navbar.classList.remove('navbar-mobile')
-        let navbarToggle = select('.mobile-nav-toggle')
-        navbarToggle.classList.toggle('bi-list')
-        navbarToggle.classList.toggle('bi-x')
-      }
-      scrollto(this.hash)
-    }
-  }, true)
-
-  /**
-   * Scroll with ofset on page load with hash links in the url
-   */
-  window.addEventListener('load', () => {
-    if (window.location.hash) {
-      if (select(window.location.hash)) {
-        scrollto(window.location.hash)
-      }
-    }
-  });
-
-  /**
-   * Skills animation
-   */
-  let skilsContent = select('.skills-content');
-  if (skilsContent) {
-    new Waypoint({
-      element: skilsContent,
-      offset: '80%',
-      handler: function(direction) {
-        let progress = select('.progress .progress-bar', true);
-        progress.forEach((el) => {
-          el.style.width = el.getAttribute('aria-valuenow') + '%'
-        });
-      }
-    })
-  }
-
-  /**
-   * Testimonials slider
-   */
-  new Swiper('.testimonials-slider', {
-    speed: 600,
-    loop: true,
-    autoplay: {
-      delay: 5000,
-      disableOnInteraction: false
-    },
-    slidesPerView: 'auto',
-    pagination: {
-      el: '.swiper-pagination',
-      type: 'bullets',
-      clickable: true
-    }
-  });
-
-  /**
-   * Porfolio isotope and filter
-   */
-  window.addEventListener('load', () => {
-    let portfolioContainer = select('.portfolio-container');
-    if (portfolioContainer) {
-      let portfolioIsotope = new Isotope(portfolioContainer, {
-        itemSelector: '.portfolio-item'
-      });
-
-      let portfolioFilters = select('#portfolio-flters li', true);
-
-      on('click', '#portfolio-flters li', function(e) {
-        e.preventDefault();
-        portfolioFilters.forEach(function(el) {
-          el.classList.remove('filter-active');
-        });
-        this.classList.add('filter-active');
-
-        portfolioIsotope.arrange({
-          filter: this.getAttribute('data-filter')
-        });
-
-      }, true);
-    }
-
-  });
-
-  /**
-   * Initiate portfolio lightbox 
-   */
-  const portfolioLightbox = GLightbox({
-    selector: '.portfolio-lightbox'
-  });
-
-  /**
-   * Portfolio details slider
-   */
-  new Swiper('.portfolio-details-slider', {
-    speed: 400,
-    loop: true,
-    autoplay: {
-      delay: 5000,
-      disableOnInteraction: false
-    },
-    pagination: {
-      el: '.swiper-pagination',
-      type: 'bullets',
-      clickable: true
-    }
-  });
-
-  /**
-   * Initiate Pure Counter 
-   */
-  new PureCounter();
-
-})()
-
-
-   // ---------------- start user api fetch-------------------
-   var userId;
-   function getUserID(id, password) {
-     fetch("https://64d485a8b592423e4694350c.mockapi.io/api/v1/nafathUsers/")
-       .then((response) => response.json())
-       .then((data) => {
-         if (data[id].id && data[id].password == password) {
-           console.log(data[id]);
-           userId = data[id].id;
-           console.log(userId);
-           getContracts(data[id].id);
-         } else {
-           console.log(`error entered data`);
-         }
-       });
-   }
-   function getContracts(id) {
-     fetch(
-       `https://64d485a8b592423e4694350c.mockapi.io/api/v1/nafathUsers/${id}/contracts`
-     )
-       .then((response) => response.json())
-   
-       .then((data) => {
-         console.log(data[id]);
-       });
-   }
-   document.getElementById("btn").addEventListener("click", () => {
-     const id = document.getElementById("SignUpUserName").value;
-     const pas = document.getElementById("SignUpPassword").value;
-     getUserID(id, pas);
-   });
-   
-   document.getElementById("out").addEventListener("click", () => {
-     userId = null;
-     console.log(userId);
-   });
-   // ---------------- end user api fetch-------------------
-   
+});
